dbec61c9f9b34878c273ab2e6f5e465887db0800
Stephanie Englberger <stephaniekoebich@aol.com>
2025-09-23 10:46:19 +0200

    Create user model

--- app_auth/models.py
+++ app_auth/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='profile_pics/', null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True, default='')
+    tel = models.CharField(max_length=20, null=True, blank=True, default='')
+    description = models.TextField(null=True, blank=True, default='')
+    working_hours = models.CharField(max_length=100, null=True, blank=True, default='')
+    
+    def __str__(self):
+        return f"{self.user.username} Profile"
