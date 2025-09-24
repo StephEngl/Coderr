fb7790053471d513fed4aa2cadb9fef6d025ca9c
Stephanie Englberger <stephaniekoebich@aol.com>
2025-09-24 10:52:09 +0200

    Add url paths for showing user information

--- app_auth/api/urls.py
+++ app_auth/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import RegistrationView, LoginView
+from .views import RegistrationView, LoginView, UserDetailView, BusinessUserListView, CustomerUserListView
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
     path('login/', LoginView.as_view(), name='login'),
+    path('profile/<int:user_id>/', UserDetailView.as_view(), name='user-detail'),
+    path('profile/business/', BusinessUserListView.as_view(), name='business-user-list'),
+    path('profile/customer/', CustomerUserListView.as_view(), name='customer-user-list'),
 ]
