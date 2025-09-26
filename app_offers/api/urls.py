7d14eb23b1941e6cedce2fe62b8a06f9e720a75a
Stephanie Englberger <stephaniekoebich@aol.com>
2025-09-26 09:59:07 +0200

    Implement offers to urls

---
+++ app_offers/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import OfferViewSet, OfferDetailView
+
+router = DefaultRouter()
+router.register(r'offers', OfferViewSet)
+
+urlpatterns = [
+    path('offerdetails/<int:pk>/', OfferDetailView.as_view(), name='offer-detail'),
+    path('', include(router.urls)),
+]
