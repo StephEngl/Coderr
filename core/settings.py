51b7bc92213aadbb2a0b379ef7780831026d8b4b
Stephanie Englberger <stephaniekoebich@aol.com>
2025-09-26 09:59:28 +0200

    Add offers app to project

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
-    'app_auth'
+    'app_auth',
+    'app_offers',
 ]
 
 MIDDLEWARE = [
@@ -143,4 +144,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 6,
 }
