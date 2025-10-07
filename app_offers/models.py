2ebab199e6c9c16f854c6cc67e2e57798c82744f
Stephanie Englberger <stephaniekoebich@aol.com>
2025-10-07 12:43:25 +0200

    Adapt offer model fields to be required

--- app_offers/models.py
+++ app_offers/models.py
@@ -9,8 +9,8 @@ class Offer(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    min_price = models.DecimalField(max_digits=10, decimal_places=2)
-    min_delivery_time = models.IntegerField(help_text="Delivery time in days")
+    min_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    min_delivery_time = models.IntegerField(help_text="Delivery time in days", null=True, blank=True)
 
     def __str__(self):
         return self.title
