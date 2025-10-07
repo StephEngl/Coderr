1db7700cf364a533e3fb226688f0336aa138339d
Stephanie Englberger <stephaniekoebich@aol.com>
2025-10-07 10:32:59 +0200

    Add some unit tests for offer section

--- app_offers/tests/test_offers.py
+++ app_offers/tests/test_offers.py
@@ -1,35 +1,73 @@
 from django.urls import reverse
 from django.contrib.auth.models import User
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
+from rest_framework.authtoken.models import Token
+
+from app_offers.models import Offer
+
+from ..api.serializers import OfferDetailSerializer, OffersSerializer
+from app_auth.models import UserProfile
+
 
 class OfferAPITests(APITestCase):
     def setUp(self):
         # Create a test user
         self.user = User.objects.create_user(username='testuser', password='testpass')
-        self.user.first_name = 'Test'
-        self.user.last_name = 'User'
-        self.user.save()
-        
-        # Authenticate the test client
-        self.client.login(username='testuser', password='testpass')
+        UserProfile.objects.create(user=self.user, type='business')
+        self.anotherUser = User.objects.create_user(username='anotheruser', password='testpass')
+        UserProfile.objects.create(user=self.anotherUser, type='business')
 
-    def test_get_offers_list(self):
+        # Authenticate the test client
+        # self.client = APIClient()
+        # self.client.login(username='testuser', password='testpass')
+        self.token = Token.objects.create(user=self.user)
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.offers = [
+            Offer.objects.create(user=self.user, title='Test Offer', description='This is a test offer.', min_price=100.00, min_delivery_time=5),
+            Offer.objects.create(user=self.anotherUser, title='Test Another Offer', description='This is another test offer.', min_price=105.00, min_delivery_time=7)
+        ]
+
+        self.offerCount = Offer.objects.all().count()
+        
+    def test_unauthorized_access(self):
         url = reverse('offers-list')
         response = self.client.get(url)
+        
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], self.offerCount)
 
-    def test_bad_request_invalid_min_price(self):
-        url = reverse('offers-list')
-        # Übergib z. B. einen ungültigen Typ für min_price (String statt float)
-        response = self.client.get(url, {'min_price': 'invalid'})
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        # self.assertIn('min_price', response.data)
+    def test_offer_properties(self):
+        offer = self.offers[0]
+        url = reverse('offers-detail', kwargs={'pk': offer.id})
+        response = self.client.get(url)
 
-    def test_bad_request_invalid_ordering(self):
-        url = reverse('offers-list')
-        # Übergib einen ungültigen Wert für ordering
-        response = self.client.get(url, {'ordering': 'invalid_field'})
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        # Optionale tiefergehende Prüfung:
-        # self.assertIn('ordering', response.data)
+        self.assertEqual(response.data['results'][0], {'id': 1, 'title': 'Test Offer', 'description': 'This is a test offer.', 'min_price': '100.00', 'min_delivery_time': 5, 'user': 1, 'image': None, 'created_at': response.data['results'][0]['created_at'], 'updated_at': response.data['results'][0]['updated_at'], 'details': [], 'user_details': {'first_name': '', 'last_name': '', 'username': 'testuser'}})
+
+    
+
+    # def test_get_offers_list(self):
+    #     Offer.objects.create(user=self.user, title='Test Offer', description='This is a test offer.', min_price=100.00, min_delivery_time=5)
+    #     Offer.objects.create(user=self.user, title='Test Offer', description='This is a test offer.', min_price=100.00, min_delivery_time=5)
+    #     url = reverse('offers-list')
+    #     response = self.client.get(url)
+    #     print(response.data)
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    # def test_create_offer(self):
+    #     url = reverse('offers-list')
+    #     data = self.offer
+    #     response = self.client.post(url, data, format='json')
+    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+    # def test_get_offer_detail(self):
+    #     url = reverse('offers-detail', kwargs={'pk': self.offer.id})
+    #     response = self.client.get(url)
+    #     expected_data = OfferDetailSerializer(self.offer).data
+
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    #     self.assertEqual(response.data, expected_data)
