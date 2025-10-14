9bba7f314fcd69ffbcbbb0a61cbb297122797179
Stephanie Englberger <stephaniekoebich@aol.com>
2025-10-14 15:10:14 +0200

    Adapt filterset to annotation

--- app_offers/api/filters.py
+++ app_offers/api/filters.py
@@ -6,9 +6,17 @@ class OfferFilter(filters.FilterSet):
     FilterSet for filtering offers based on minimum price and delivery time.
     """
     creator_id = filters.NumberFilter(field_name='user__id', lookup_expr='exact')
-    min_price = filters.NumberFilter(field_name='min_price', lookup_expr='gte')
-    max_delivery_time = filters.NumberFilter(field_name='max_delivery_time', lookup_expr='lte')
+    min_price = filters.NumberFilter(method='filter_min_price')
+    min_delivery_time = filters.NumberFilter(method='filter_min_delivery_time')
 
     class Meta:
         model = Offer
-        fields = ['creator_id', 'min_price', 'max_delivery_time']
+        fields = ['creator_id', 'min_price', 'min_delivery_time']
+    
+    def filter_min_price(self, queryset, name, value):
+        # Filter auf den annotierten Wert min_price >= value
+        return queryset.filter(min_price__gte=value)
+
+    def filter_min_delivery_time(self, queryset, name, value):
+        # Filter auf den annotierten Wert min_delivery_time >= value
+        return queryset.filter(min_delivery_time__gte=value)
