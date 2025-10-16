a221983c6cdf8516fc692093edf0383755ae4341
Stephanie Englberger <stephaniekoebich@aol.com>
2025-10-16 17:14:20 +0200

    Add messages to offer permissions

--- app_offers/api/permissions.py
+++ app_offers/api/permissions.py
@@ -6,6 +6,7 @@ class IsBusinessUser(permissions.BasePermission):
     """
     Custom permission to only allow business users to access certain views.
     """
+    message = "Only business users are allowed to perform this action."
 
     def has_permission(self, request, view):
         # return request.user and request.user.is_authenticated and getattr(request.user, 'type', None) == 'business'
@@ -22,6 +23,7 @@ class IsOwnerOfOffer(permissions.BasePermission):
     """
     Custom permission to only allow owners of an offer to edit or delete it.
     """
+    message = "Only the offer owner may edit or delete this offer."
 
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
