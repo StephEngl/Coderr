b9b3d9d9986d385b642ca4a1face310b9e84a0ed
Stephanie Englberger <stephaniekoebich@aol.com>
2025-10-22 22:19:19 +0200

    Adapt order modell

--- app_orders/models.py
+++ app_orders/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
-from app_offers.models import OfferDetail
 
 
-class Order(OfferDetail):
+class Order(models.Model):
     STATUS_CHOICES = [
         ('in_progress', 'In Progress'),
         ('completed', 'Completed'),
         ('cancelled', 'Cancelled'),
     ]
+
+    title = models.CharField(max_length=255)
+    revisions = models.IntegerField(default=0)
+    delivery_time_in_days = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    offer_type = models.CharField(max_length=30, default='basic')
+    features = models.JSONField(default=list)
 
     customer_user = models.ForeignKey(User, related_name='orders_as_customer', on_delete=models.CASCADE)
     business_user = models.ForeignKey(User, related_name='orders_as_business', on_delete=models.CASCADE)
