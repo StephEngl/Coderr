bac809be3a6e6e5f53e6f2edf39d587ccb15fa23
Stephanie Englberger <stephaniekoebich@aol.com>
2025-11-04 16:41:48 +0100

    Adapt url-patterns for reviews

---
+++ app_reviews/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import ReviewViewSet
+
+router = DefaultRouter()
+router.register(r'reviews', ReviewViewSet, basename='reviews')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
