95e6eb7e0b5c7145698654ed89ba3b5b38a7abac
Stephanie Englberger <stephaniekoebich@aol.com>
2025-11-04 14:46:25 +0100

    Extend order urls with (completed) order counts

--- app_orders/api/urls.py
+++ app_orders/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import OrderViewSet
+from .views import OrderViewSet, OrderCountView, CompletedOrderCountView
 
 router = DefaultRouter()
 router.register(r'orders', OrderViewSet, basename='orders')
 
 urlpatterns = [
+    path('order-count/<int:pk>/', OrderCountView.as_view(), name='order-count'),
+    path('completed-order-count/<int:pk>/', CompletedOrderCountView.as_view(), name='completed-order-count'),
     path('', include(router.urls)),
 ]
