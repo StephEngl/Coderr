225b19b929d607b44307a9248e40522148fcf886
Stephanie Englberger <stephaniekoebich@aol.com>
2025-11-04 16:41:10 +0100

    Implement list and create serializer for reviews

---
+++ app_reviews/api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from ..models import Review
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['id', 'business_user', 'reviewer', 'rating', 'description', 'created_at', 'updated_at']
+
+
+class ReviewCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['business_user', 'rating', 'description']
