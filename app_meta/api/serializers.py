e1cf0c1e9dcb1e3bc1be8585cf1830d2d80c19ae
Stephanie Englberger <stephaniekoebich@aol.com>
2025-11-11 11:05:29 +0100

    Adapt base info serializer for showing rating

--- app_meta/api/serializers.py
+++ app_meta/api/serializers.py
@@ -5,6 +5,6 @@ class BaseInfoSerializer(serializers.Serializer):
     Serializer for basic information about an entity.
     """
     review_count = serializers.IntegerField(read_only=True, help_text="Total number of reviews on the platform.")
-    average_count = serializers.DecimalField(read_only=True, max_digits=2, decimal_places=1, help_text="Average rating across all reviews.")
+    average_rating = serializers.DecimalField(read_only=True, max_digits=2, decimal_places=1, coerce_to_string=False, help_text="Average rating across all reviews.")
     business_profile_count = serializers.IntegerField(read_only=True, help_text="Total number of business profiles registered.")
     offer_count = serializers.IntegerField(read_only=True, help_text="Total number of offers available on the platform.")
