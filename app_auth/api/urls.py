3f83d1e347522ee3b3f05b3b83b66f73199e8a0e
Stephanie Englberger <stephaniekoebich@aol.com>
2025-11-11 10:06:30 +0100

    Fix endpoint profiles urls

--- app_auth/api/urls.py
+++ app_auth/api/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/<int:user_id>/', UserDetailView.as_view(), name='user-detail'),
-    path('profile/business/', BusinessUserListView.as_view(), name='business-user-list'),
-    path('profile/customer/', CustomerUserListView.as_view(), name='customer-user-list'),
+    path('profiles/business/', BusinessUserListView.as_view(), name='business-user-list'),
+    path('profiles/customer/', CustomerUserListView.as_view(), name='customer-user-list'),
 ]
